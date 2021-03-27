2be67f57f8e002b922094ffd0b21888042dadb65
Eccentricware <zeldark@gmail.com>
2021-03-27 12:48:44 -0700

    Initialize please

--- src/App.js
+++ src/App.js
@@ -34,11 +34,21 @@ function App() {
     Optimizer.findEVContributionOfCrewToCite();
     Optimizer.sortCrewToCite();
     console.log(Optimizer);
+    console.log("Ranked Crew to Train");
+    console.log(Optimizer.rankedCrewToTrain);
+    console.log("Ranked Crew To Cite");
+    console.log(Optimizer.rankedCrewToCite);
   }
 
   return (
     <div className="App">
       <SaveImport label={saveData} importData={importData} optimizer={Optimizer}/>
+      <div>Just copy-paste your save file into the field like you would on DataCore.</div>
+      <div>Scroll to the bottom where you can find your ranked crew to train and cite!</div>
+      <div>Game roster will be constantly synced after migration to <a href='https://datacore.app/' className="link">DataCore!</a></div>
+      <div>There are tons of other resources there in the meantime, check it out!</div>
+      <div>---</div>
+      <div>Last Game Roster Update: 3-27-2021</div>
     </div>
   );
 }
